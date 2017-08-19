c40cb35478922866d0b827f24b48ca1ad8b3768e
Animania260 <animania260@gmail.com>
2017-08-18 23:41:18 -0400

    Revert "GPU OC to 550 MHz for Samsung msm8929."
    This reverts commit 1c2c27915f338ae96d22ffcfe3f6a3066499eb13.

--- drivers/clk/qcom/clock-gcc-8916.c
+++ drivers/clk/qcom/clock-gcc-8916.c
@@ -554,7 +554,6 @@ static struct clk_freq_tbl ftbl_gcc_camss_vfe0_clk[] = {
 	F( 320000000,	   gpll0, 2.5,	  0,	0),
 	F( 400000000,	   gpll0,   2,	  0,	0),
 	F( 465000000,	   gpll2,   2,	  0,	0),
-	F( 550000000,	   gpll2,   2,	  0,	0),
 	F_END
 };
 
@@ -568,12 +567,12 @@ static struct rcg_clk vfe0_clk_src = {
 		.dbg_name = "vfe0_clk_src",
 		.ops = &clk_ops_rcg,
 		VDD_DIG_FMAX_MAP3(LOW, 160000000, NOMINAL, 320000000, HIGH,
-			550000000),
+			465000000),
 		CLK_INIT(vfe0_clk_src.c),
 	},
 };
 
-static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_550_clk[] = {
+static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_465_clk[] = {
 	F(  19200000,	      xo,   1,	  0,	0),
 	F(  50000000,  gpll0_aux,  16,	  0,	0),
 	F(  80000000,  gpll0_aux,  10,	  0,	0),
@@ -586,7 +585,6 @@ static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_550_clk[] = {
 	F( 310000000,	   gpll2,   3,	  0,	0),
 	F( 400000000,  gpll0_aux,   2,	  0,	0),
 	F( 465000000,      gpll2,   2,	  0,	0),
-	F( 550000000,      gpll2,   2,	  0,	0),
 	F_END
 };
 
@@ -602,8 +600,6 @@ static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_clk[] = {
 	F( 294912000,	   gpll1,   3,	  0,	0),
 	F( 310000000,	   gpll2,   3,	  0,	0),
 	F( 400000000,  gpll0_aux,   2,	  0,	0),
-	F( 465000000,      gpll2,   2,	  0,	0),
-	F( 550000000,      gpll2,   2,	  0,	0),
 	F_END
 };
 
@@ -617,7 +613,7 @@ static struct rcg_clk gfx3d_clk_src = {
 		.dbg_name = "gfx3d_clk_src",
 		.ops = &clk_ops_rcg,
 		VDD_DIG_FMAX_MAP3(LOW, 200000000, NOMINAL, 310000000, HIGH,
-			550000000),
+			400000000),
 		CLK_INIT(gfx3d_clk_src.c),
 	},
 };
@@ -1002,7 +998,7 @@ static struct rcg_clk csi1phytimer_clk_src = {
 static struct clk_freq_tbl ftbl_gcc_camss_cpp_clk[] = {
 	F( 160000000,	   gpll0,   5,	  0,	0),
 	F( 320000000,	   gpll0, 2.5,	  0,	0),
-	F( 550000000,	   gpll2,   2,	  0,	0),
+	F( 465000000,	   gpll2,   2,	  0,	0),
 	F_END
 };
 
@@ -1016,7 +1012,7 @@ static struct rcg_clk cpp_clk_src = {
 		.dbg_name = "cpp_clk_src",
 		.ops = &clk_ops_rcg,
 		VDD_DIG_FMAX_MAP3(LOW, 160000000, NOMINAL, 320000000, HIGH,
-			550000000),
+			465000000),
 		CLK_INIT(cpp_clk_src.c),
 	},
 };
@@ -2803,8 +2799,8 @@ static void gcc_gfx3d_fmax(struct platform_device *pdev)
 	pr_info("%s, Version: %d, bin: %d\n", __func__, version,
 					bin);
 
-	gfx3d_clk_src.c.fmax[VDD_DIG_HIGH] = 550000000;
-	gfx3d_clk_src.freq_tbl = ftbl_gcc_oxili_gfx3d_550_clk;
+	gfx3d_clk_src.c.fmax[VDD_DIG_HIGH] = 465000000;
+	gfx3d_clk_src.freq_tbl = ftbl_gcc_oxili_gfx3d_465_clk;
 }
 
 static int msm_gcc_probe(struct platform_device *pdev)
